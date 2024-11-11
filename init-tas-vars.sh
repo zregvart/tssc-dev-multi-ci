2ae2a6b492d1e09dae451d5b088432e1dab9fa07
John Duimovich <jduimovich@gmail.com>
2024-11-11 12:52:25 -0500

    Update init-tas-vars.sh
    Co-authored-by: Adam Cmiel <acmiel@redhat.com>

--- init-tas-vars.sh
+++ init-tas-vars.sh
@@ -6,7 +6,7 @@ echo "Checking for REKOR and TUF"
 # get hosts, if errors occur or cannot find routes, set to none
 RT=$(oc get routes -n rhtap-tas -o name 2>  /dev/null | grep rekor-server )
 if [[ $RT == "" ]]; then 
-    export REKOR_HOST=none 
+    export REKOR_HOST=''
 else
     HOST=$(oc get -n rhtap-tas $RT -o jsonpath={.spec.host})
     export REKOR_HOST=https://$HOST
@@ -14,7 +14,7 @@ fi
 
 RT=$(oc get routes -n rhtap-tas -o name 2>  /dev/null| grep tuf)
 if [[ $RT == "" ]]; then 
-    export TUF_MIRROR=none 
+    export TUF_MIRROR='' 
 else
     HOST=$(oc get -n rhtap-tas $RT -o jsonpath={.spec.host})
     export TUF_MIRROR=https://$HOST
